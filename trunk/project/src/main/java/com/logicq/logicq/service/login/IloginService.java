d5e4ca8f09cc36c77f75e9364f7596960c6fd1db
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-01-26 05:40:53 +0000

    src deleted

--- trunk/project/src/main/java/com/logicq/logicq/service/login/IloginService.java
+++
@@ -1,9 +0,0 @@
-package com.logicq.logicq.service.login;
-
-import com.logicq.logicq.ui.login.vo.LoginRequest;
-import com.logicq.logicq.ui.login.vo.LoginResponse;
-
-public interface IloginService {
-	public LoginResponse checkLoginUser(LoginRequest p_request,LoginResponse p_loginresponse);
-
-}
