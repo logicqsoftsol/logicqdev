b06c068ff20002a756e03849395e003524d09332
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-01-26 05:40:53 +0000

    src deleted

--- project/src/main/java/com/logicq/logicq/dao/map/ILocationMapDAO.java
+++
@@ -1,9 +0,0 @@
-package com.logicq.logicq.dao.map;
-
-import com.logicq.logicq.model.map.LocationMap;
-
-public interface ILocationMapDAO {
-	
-	LocationMap getLocation(String ipAddress) throws Exception;
-
-}
