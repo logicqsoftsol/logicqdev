f9ff70b62172b81037a48d87a4c0eeabcc3cc4d7
niharranjanbehera <niharranjanbehera@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-01-31 19:01:36 +0000

    location table query

--- project/db/location.sql
+++ project/db/location.sql
@@ -1,12 +1,14 @@
-CREATE
-  TABLE location
+ CREATE TABLE location
   (
-    locationid   INT(10),
+    locationid   INT (10),
     locationname VARCHAR(60) NOT NULL,
+	locality VARCHAR(60),
+	pincode BIGINT (16),
     UNIQUE INDEX (locationid),
-    UNIQUE INDEX (locationname),
-    PRIMARY KEY (locationid)
-  ) ;
-  
-  //insert query
-insert into location values(1,'Pune');
+    UNIQUE INDEX (pincode),
+    PRIMARY KEY (locationid,locationname,locality,pincode) ) ;
+    
+    
+//insert Query
+
+ insert into location values(1,'Bhbaneswar','SaheedNagar',754879);
