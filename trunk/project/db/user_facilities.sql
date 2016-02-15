19765b4529805beacea755ccd55c78f1fc304308
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-02-15 15:42:38 +0000

    changes for  created by and updateby and cration time and updatime

--- trunk/project/db/user_facilities.sql
+++ trunk/project/db/user_facilities.sql
@@ -1,6 +1,10 @@
 CREATE TABLE user_facilities (
   facility_id bigint(10) NOT NULL AUTO_INCREMENT,
   facility_name varchar(45) NOT NULL,
+  last_update TIMESTAMP,
+  create_time TIMESTAMP,
+  created_by  VARCHAR(60),
+  updated_by VARCHAR(60),
   PRIMARY KEY (facility_id)
 )
 /
