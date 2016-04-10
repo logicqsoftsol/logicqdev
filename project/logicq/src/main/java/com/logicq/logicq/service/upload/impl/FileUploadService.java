a9e1a6aa1b3839a7110f20ad95ce7918f82da9c1
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-10 19:16:40 +0000

    modified units for image save.

--- project/logicq/src/main/java/com/logicq/logicq/service/upload/impl/FileUploadService.java
+++ project/logicq/src/main/java/com/logicq/logicq/service/upload/impl/FileUploadService.java
@@ -1,10 +1,18 @@
-
 package com.logicq.logicq.service.upload.impl;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.logicq.logicq.conversion.fileupload.FileUploadConversion;
 import com.logicq.logicq.dao.upload.IFileUploadDAO;
+import com.logicq.logicq.model.upload.UploadFile;
 import com.logicq.logicq.service.upload.IFileUploadService;
 import com.logicq.logicq.ui.fileupload.vo.FileUploadRequest;
 import com.logicq.logicq.ui.fileupload.vo.FileUploadResponse;
@@ -27,7 +35,23 @@ public class FileUploadService implements IFileUploadService {
 
 	public void save(FileUploadRequest uploadFileRequest, FileUploadResponse uploadFileResponse) {
 
-		fileUploadDAO.save(fileuploadConversion.handleConversionVOtoEntityWithContext(uploadFileRequest.getFileupload()));
+		UploadFile uploadFile = fileuploadConversion.handleConversionVOtoEntityWithContext(uploadFileRequest.getFileupload());
+		ImageInputStream input = null;
+		byte[] data = uploadFileRequest.getFileupload().getData();
+		try {
+			String path = "D:\\image\\" + uploadFile.getUserId() + ".jpg";
+			File dest = new File(path);
+			input = ImageIO.createImageInputStream(new ByteArrayInputStream(data));
+			BufferedImage image = ImageIO.read(input);
+			boolean isImageWritten = ImageIO.write(image, "jpg", dest);
+			if (isImageWritten) {
+				uploadFile.setQualifiedPath(path);
+				fileUploadDAO.save(uploadFile);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+		}
 	}
 
 	public void delete(FileUploadRequest uploadFileRequest, FileUploadResponse uploadFileResponse) {
