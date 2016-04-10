1a3b97e7ee0aa847580981b6c4a4c3adfe579e43
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-10 19:14:40 +0000

    modified units for image save.

--- trunk/project/logicq/src/main/java/com/logicq/logicq/conversion/fileupload/FileUploadConversion.java
+++ trunk/project/logicq/src/main/java/com/logicq/logicq/conversion/fileupload/FileUploadConversion.java
@@ -5,47 +5,46 @@ import com.logicq.logicq.model.upload.UploadFile;
 import com.logicq.logicq.ui.fileupload.vo.FileUploadVO;
 
 public class FileUploadConversion {
-	
+
 	private static FileUploadConversion instance;
 
 	public static FileUploadConversion getInstance() {
+
 		if (null == instance) {
 			instance = new FileUploadConversion();
 		}
 		return instance;
 	}
-	
 
 	public UploadFile conversionFromVOtoEntity(FileUploadVO fileUploadVO, UploadFile uploadFile) {
-		uploadFile.setFileName(fileUploadVO.getFileName());
-		uploadFile.setData(fileUploadVO.getData());
-		uploadFile.setFilesize(fileUploadVO.getFileSize());
+
+		uploadFile.setFileName(fileUploadVO.getUserid() + ".jpg");
+		uploadFile.setFileSize(fileUploadVO.getFileSize());
 		uploadFile.setFileType(fileUploadVO.getFileType());
-		uploadFile.setUserid(fileUploadVO.getUserid());
-		uploadFile.setFileuploadDate(fileUploadVO.getUploadDate());
+		uploadFile.setUserId(fileUploadVO.getUserid());
+		uploadFile.setFileUploadDate(fileUploadVO.getUploadDate());
 		return uploadFile;
 	}
 
-	public FileUploadVO conversionFromEntitytoVO( UploadFile uploadFile, FileUploadVO fileUploadVO) {
+	public FileUploadVO conversionFromEntitytoVO(UploadFile uploadFile, FileUploadVO fileUploadVO) {
+
 		fileUploadVO.setFileName(uploadFile.getFileName());
-		fileUploadVO.setData(uploadFile.getData());
-		fileUploadVO.setFileSize(uploadFile.getFilesize());
+		fileUploadVO.setFileSize(uploadFile.getFileSize());
 		fileUploadVO.setFileType(uploadFile.getFileType());
-		fileUploadVO.setUserid(uploadFile.getUserid());
-		fileUploadVO.setUploadDate(uploadFile.getFileuploadDate());
+		fileUploadVO.setUserid(uploadFile.getUserId());
+		fileUploadVO.setUploadDate(uploadFile.getFileUploadDate());
 		return fileUploadVO;
 	}
-	
+
 	public UploadFile handleConversionVOtoEntityWithContext(FileUploadVO fileUploadVO) {
+
 		UploadFile uploadFile = null;
 		if (null != LogicqContextProvider.getApplicationContext()) {
 			uploadFile = LogicqContextProvider.getApplicationContext().getBean(UploadFile.class);
 		}
-		if(null==uploadFile){
-			uploadFile=new UploadFile();
+		if (null == uploadFile) {
+			uploadFile = new UploadFile();
 		}
 		return conversionFromVOtoEntity(fileUploadVO, uploadFile);
 	}
-
-
 }
