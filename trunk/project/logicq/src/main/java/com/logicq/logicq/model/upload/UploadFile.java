333381b57ba42bf223c5778f731292b44a7879ed
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-10 19:15:46 +0000

    modified units for image save.

--- trunk/project/logicq/src/main/java/com/logicq/logicq/model/upload/UploadFile.java
+++ trunk/project/logicq/src/main/java/com/logicq/logicq/model/upload/UploadFile.java
@@ -1,4 +1,5 @@
 package com.logicq.logicq.model.upload;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -7,113 +8,100 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table; 
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "FILES_UPLOAD")
+@Table(name = "FILE_UPLOAD")
 public class UploadFile implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8806356173783436068L;
 	@Id
 	@Column(name = "FILE_ID", unique = true, nullable = false)
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
 	@Column(name = "FILE_NAME")
 	private String fileName;
-	
-	@Column(name = "FILE_DATA")
-	private byte[] data;
-    
 	@Column(name = "FILE_SIZE")
-	private String filesize;
-	
+	private String fileSize;
 	@Column(name = "FILE_TYPE")
 	private String fileType;
-	
 	@Column(name = "FILE_UPLOAD_DATE")
-	private Date fileuploadDate;
-	
-	@Column(name = "USERID")
-	private Integer userid;
-	
-	
-	public Integer getUserid() {
-	
-		return userid;
-	}
-	
-	public void setUserid(Integer userid) {
-	
-		this.userid = userid;
+	private Date fileUploadDate;
+	@Column(name = "USER_ID")
+	private Long userId;
+	@Column(name = "QUALIFIED_PATH")
+	private String qualifiedPath;
+
+	public String getFileSize() {
+
+		return fileSize;
 	}
 
+	public void setFileSize(String fileSize) {
 
-	public Date getFileuploadDate() {
-	
-		return fileuploadDate;
+		this.fileSize = fileSize;
 	}
 
-	
-	public void setFileuploadDate(Date fileuploadDate) {
-	
-		this.fileuploadDate = fileuploadDate;
+	public Date getFileUploadDate() {
+
+		return fileUploadDate;
 	}
 
-	public String getFilesize() {
-	
-		return filesize;
+	public void setFileUploadDate(Date fileUploadDate) {
+
+		this.fileUploadDate = fileUploadDate;
 	}
-	
-	public void setFilesize(String filesize) {
-	
-		this.filesize = filesize;
+
+	public Long getUserId() {
+
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+
+		this.userId = userId;
+	}
+
+	public String getQualifiedPath() {
+
+		return qualifiedPath;
+	}
+
+	public void setQualifiedPath(String qualifiedPath) {
+
+		this.qualifiedPath = qualifiedPath;
 	}
 
 	public String getFileType() {
-	
+
 		return fileType;
 	}
 
-
-	
 	public void setFileType(String fileType) {
-	
+
 		this.fileType = fileType;
 	}
 
-
 	public long getId() {
-	
+
 		return id;
 	}
 
-	
 	public void setId(long id) {
-	
+
 		this.id = id;
 	}
 
-	
 	public String getFileName() {
-	
+
 		return fileName;
 	}
 
-	
 	public void setFileName(String fileName) {
-	
+
 		this.fileName = fileName;
 	}
-
-	
-	public byte[] getData() {
-	
-		return data;
-	}
-
-	
-	public void setData(byte[] data) {
-	
-		this.data = data;
-	}
-
 }
