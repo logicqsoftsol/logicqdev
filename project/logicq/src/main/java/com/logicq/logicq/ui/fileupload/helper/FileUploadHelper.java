f7d54f1d90d9edee3a75373b2ebe3c8b797f3902
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-10 19:17:36 +0000

    modified units for image save.

--- project/logicq/src/main/java/com/logicq/logicq/ui/fileupload/helper/FileUploadHelper.java
+++ project/logicq/src/main/java/com/logicq/logicq/ui/fileupload/helper/FileUploadHelper.java
@@ -14,44 +14,46 @@ import com.logicq.logicq.ui.fileupload.vo.FileUploadResponse;
 import com.logicq.logicq.ui.fileupload.vo.FileUploadVO;
 
 public class FileUploadHelper {
-	
+
 	@Autowired
 	IFileUploadFacade fileUploadFacade;
 
-	
 	public IFileUploadFacade getFileUploadFacade() {
-	
+
 		return fileUploadFacade;
 	}
 
-	
 	public void setFileUploadFacade(IFileUploadFacade fileUploadFacade) {
-	
+
 		this.fileUploadFacade = fileUploadFacade;
 	}
-	
-	public void uploadDocument(FileUploadRequest uploadFileRequest,FileUploadResponse uploadFileResponse){
+
+	public void uploadDocument(FileUploadRequest uploadFileRequest, FileUploadResponse uploadFileResponse) {
+
 		fileUploadFacade.save(uploadFileRequest, uploadFileResponse);
 	}
-	
+
 	public static FileUploadBean conversionVOtoUI(FileUploadVO uploadvo, FileUploadBean uploadbean) {
+
 		return uploadbean;
 	}
-	
+
 	public static FileUploadVO conversionUItoVO(FileUploadBean uploadbean, FileUploadVO uploadvo) {
+
 		try {
-		UploadedFile fileuploaded = uploadbean.getFile();
-		InputStream inputStream;
-		inputStream = fileuploaded.getInputstream();
-		byte[] fileuploaddata = new byte[4096];
-		inputStream.read(fileuploaddata);
-		uploadvo.setData(fileuploaddata);
-		uploadvo.setFileType(uploadbean.getFile().getContentType());
-		uploadvo.setFileName(uploadbean.getFile().getFileName());
-		uploadvo.setFileSize(uploadbean.getFile().getSize()+"KB");
-		uploadvo.setUploadDate(new Date());
-		uploadvo.setUserid(01101);
-		inputStream.close();
+			UploadedFile fileuploaded = uploadbean.getFile();
+			InputStream inputStream;
+			inputStream = fileuploaded.getInputstream();
+			byte[] fileuploaddata = new byte[5120000];
+			inputStream.read(fileuploaddata);
+			uploadvo.setData(fileuploaddata);
+			uploadvo.setFileType(uploadbean.getFile().getContentType());
+			uploadvo.setFileName(uploadbean.getFile().getFileName());
+			uploadvo.setFileSize(uploadbean.getFile().getSize() + "KB");
+			uploadvo.setUploadDate(new Date());
+			uploadvo.setUserid(Long.valueOf(1000));
+			uploadvo.setInputStream(inputStream);
+			//inputStream.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
