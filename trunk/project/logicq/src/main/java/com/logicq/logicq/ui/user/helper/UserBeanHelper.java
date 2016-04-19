be2ad63418b08f9ab625348b5d72a8b0525115e3
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-19 21:13:03 +0000

    changes done for upload images.

--- trunk/project/logicq/src/main/java/com/logicq/logicq/ui/user/helper/UserBeanHelper.java
+++ trunk/project/logicq/src/main/java/com/logicq/logicq/ui/user/helper/UserBeanHelper.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.logicq.logicq.facade.user.IUserFacade;
 import com.logicq.logicq.ui.user.bean.UserBean;
+import com.logicq.logicq.ui.user.vo.UserProfilesRequest;
+import com.logicq.logicq.ui.user.vo.UserProfilesResponse;
 import com.logicq.logicq.ui.user.vo.UserRegistrationRequest;
 import com.logicq.logicq.ui.user.vo.UserRegistrationResponse;
 import com.logicq.logicq.ui.user.vo.UserVO;
-
 
 public class UserBeanHelper {
 
@@ -15,20 +16,22 @@ public class UserBeanHelper {
 	IUserFacade userFacade;
 
 	public IUserFacade getUserFacade() {
+
 		return userFacade;
 	}
 
 	public void setUserFacade(IUserFacade userFacade) {
+
 		this.userFacade = userFacade;
 	}
 
 	public UserRegistrationResponse addUser(UserRegistrationRequest userRequest, UserRegistrationResponse userResponse) {
-		
-		return(userFacade.addUser(userRequest, userResponse));
+
+		return (userFacade.addUser(userRequest, userResponse));
 	}
 
 	public UserVO conversionUItoVO(UserBean userBean, UserVO userVO) {
-		
+
 		userVO.setName(userBean.getName());
 		userVO.setSurname(userBean.getSurname());
 		userVO.setEmailId(userBean.getEmail());
@@ -38,15 +41,21 @@ public class UserBeanHelper {
 		return userVO;
 	}
 
-	public UserBean conversionVOtoUI(UserVO p_user,UserBean p_userbean ) {
+	public UserBean conversionVOtoUI(UserVO p_user, UserBean p_userbean) {
+
 		p_userbean.setId(p_user.getId());
 		p_userbean.setName(p_user.getName());
 		p_userbean.setSurname(p_user.getSurname());
 		return p_userbean;
 	}
-	
-	public boolean validateUser(String user){
-		
+
+	public boolean validateUser(String user) {
+
 		return false;
+	}
+
+	public UserProfilesResponse getParticularUsersForArea(UserProfilesRequest request, UserProfilesResponse response) {
+
+		return (userFacade.getParticularUsersForArea(request, response));
 	}
 }
