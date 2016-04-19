ca75618b96158dd130961d8755bc9c86ad68bd5f
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-19 21:10:34 +0000

    changes done for upload images.

---
+++ trunk/project/logicq/src/main/java/com/logicq/logicq/ui/fileupload/properties/FileUploadImageProperties.java
@@ -0,0 +1,16 @@
+package com.logicq.logicq.ui.fileupload.properties;
+
+public class FileUploadImageProperties {
+
+	private String destinationPath;
+
+	public String getDestinationPath() {
+
+		return destinationPath;
+	}
+
+	public void setDestinationPath(String destinationPath) {
+
+		this.destinationPath = destinationPath;
+	}
+}
