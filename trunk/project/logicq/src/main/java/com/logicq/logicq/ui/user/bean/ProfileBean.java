abe1e7628dd0702fbf3195ea0489275e780f91b2
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-19 23:37:37 +0000

    added progress bar for complete event step

--- trunk/project/logicq/src/main/java/com/logicq/logicq/ui/user/bean/ProfileBean.java
+++ trunk/project/logicq/src/main/java/com/logicq/logicq/ui/user/bean/ProfileBean.java
@@ -1,11 +1,13 @@
 package com.logicq.logicq.ui.user.bean;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.primefaces.model.StreamedContent;
 import org.springframework.util.StringUtils;
@@ -30,6 +32,82 @@ public class ProfileBean {
 	private EntityAvailabilityBean avilablityDetails;
 	private String schduleformrender = "false";
 	private String userImage;
+    private String qualificationDetails;
+    private boolean addressRendered;
+    private boolean profileRendered;
+    private boolean reportRendered;
+    private boolean scheduleRendered;
+    private boolean recenthistoryRendered;
+    private boolean walletRendered;
+    private boolean facilityRendered;
+	private List<String> ratings;
+   
+	public boolean isFacilityRendered() {
+		return facilityRendered;
+	}
+
+	public void setFacilityRendered(boolean facilityRendered) {
+		this.facilityRendered = facilityRendered;
+	}
+	
+	public boolean isProfileRendered() {
+		return profileRendered;
+	}
+
+	public List<String> getRatings() {
+		ratings=new ArrayList<String>();
+		ratings.add("1");
+		ratings.add("2");
+		return ratings;
+	}
+
+	public void setRatings(List<String> ratings) {
+		this.ratings = ratings;
+	}
+
+	public void setProfileRendered(boolean profileRendered) {
+		this.profileRendered = profileRendered;
+	}
+
+	public boolean isReportRendered() {
+		return reportRendered;
+	}
+
+	public void setReportRendered(boolean reportRendered) {
+		this.reportRendered = reportRendered;
+	}
+
+	public boolean isScheduleRendered() {
+		return scheduleRendered;
+	}
+
+	public void setScheduleRendered(boolean scheduleRendered) {
+		this.scheduleRendered = scheduleRendered;
+	}
+
+	public boolean isRecenthistoryRendered() {
+		return recenthistoryRendered;
+	}
+
+	public void setRecenthistoryRendered(boolean recenthistoryRendered) {
+		this.recenthistoryRendered = recenthistoryRendered;
+	}
+
+	public boolean isWalletRendered() {
+		return walletRendered;
+	}
+
+	public void setWalletRendered(boolean walletRendered) {
+		this.walletRendered = walletRendered;
+	}
+
+	public boolean isAddressRendered() {
+		return addressRendered;
+	}
+
+	public void setAddressRendered(boolean addressRendered) {
+		this.addressRendered = addressRendered;
+	}
 
 	public String getUserImage() {
 
@@ -39,6 +117,14 @@ public class ProfileBean {
 	public void setUserImage(String userImage) {
 
 		this.userImage = userImage;
+	}
+
+	public String getQualificationDetails() {
+		return qualificationDetails;
+	}
+
+	public void setQualificationDetails(String qualificationDetails) {
+		this.qualificationDetails = qualificationDetails;
 	}
 
 	public String getSchduleformrender() {
@@ -192,5 +278,64 @@ public class ProfileBean {
 		// return new DefaultStreamedContent(new ByteArrayInputStream(null));
 		// }
 		return null;
+	}
+	
+	private void defaultProfileRenderProperty() {
+		setProfileRendered(false);
+		setAddressRendered(false);
+		setReportRendered(false);
+		setScheduleRendered(false);
+		setWalletRendered(false);
+		setRecenthistoryRendered(false);
+		setFacilityRendered(false);
+	}
+	
+//	private boolean profileRendered;
+ //   private boolean reportRendered;
+  //  private boolean scheduleRendered;
+  //  private boolean recenthistoryRendered;
+  //  private boolean walletRendered;
+    
+	public void profileDetailsDisplay(ActionEvent action) {
+		System.out.println("Test Profile");
+		defaultProfileRenderProperty();
+		setProfileRendered(true);
+		
+	}
+
+	public void addressDetailsDisplay(ActionEvent action) {
+		System.out.println("Test Address");
+		defaultProfileRenderProperty();
+		setAddressRendered(true);
+	}
+
+	public void reportDetailsDisplay(ActionEvent action) {
+		System.out.println("Test report");
+		defaultProfileRenderProperty();
+		setReportRendered(true);
+	}
+
+	public void setupScheduleDetailsDisplay(ActionEvent action) {
+		System.out.println("Test scudule");
+		defaultProfileRenderProperty();
+		setScheduleRendered(true);
+	}
+
+	public void walletDetailsDisplay(ActionEvent action) {
+		System.out.println("Test wallet");
+		defaultProfileRenderProperty();
+		setWalletRendered(true);
+	}
+
+	public void historyDetailsDisplay(ActionEvent action) {
+		System.out.println("Test history");
+		defaultProfileRenderProperty();
+		setRecenthistoryRendered(true);
+	}
+	
+	public void facilityDetailsDisplay(ActionEvent action) {
+		System.out.println("Test history");
+		defaultProfileRenderProperty();
+		setFacilityRendered(true);
 	}
 }
