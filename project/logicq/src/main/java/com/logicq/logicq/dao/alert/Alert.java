aba5f5754244e4e1ccbb632fd88f5a28d4d74feb
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-04-23 19:50:53 +0000

    changes done for alerts.

---
+++ project/logicq/src/main/java/com/logicq/logicq/dao/alert/Alert.java
@@ -0,0 +1,69 @@
+package com.logicq.logicq.dao.alert;
+
+import java.io.Serializable;
+
+import com.logicq.logicq.common.criteriamanager.BaseEntity;
+import com.logicq.logicq.constant.AlertType;
+
+public class Alert extends BaseEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7087659491456021215L;
+	private Long alertId;
+	private String alertName;
+	private AlertType alertType;
+	private Long userId;
+	private String alertStatus;
+
+	public Long getAlertId() {
+
+		return alertId;
+	}
+
+	public String getAlertName() {
+
+		return alertName;
+	}
+
+	public AlertType getAlertType() {
+
+		return alertType;
+	}
+
+	public Long getUserId() {
+
+		return userId;
+	}
+
+	public String getAlertStatus() {
+
+		return alertStatus;
+	}
+
+	public void setAlertId(Long alertId) {
+
+		this.alertId = alertId;
+	}
+
+	public void setAlertName(String alertName) {
+
+		this.alertName = alertName;
+	}
+
+	public void setAlertType(AlertType alertType) {
+
+		this.alertType = alertType;
+	}
+
+	public void setUserId(Long userId) {
+
+		this.userId = userId;
+	}
+
+	public void setAlertStatus(String alertStatus) {
+
+		this.alertStatus = alertStatus;
+	}
+}
