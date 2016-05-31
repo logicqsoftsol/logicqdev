2cee5394ef16b96e63946f486b26267e46f0e0bd
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-05-31 22:53:37 +0000

    changes for login and logout

--- logicq-html/logicq/src/main/java/com/logicq/logicq/security/config/SpringSecurityConfig.java
+++ logicq-html/logicq/src/main/java/com/logicq/logicq/security/config/SpringSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.logicq.logicq.security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -9,7 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
