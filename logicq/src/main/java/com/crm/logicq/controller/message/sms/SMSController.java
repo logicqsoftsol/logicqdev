c46a32c2a120654e0e91734a9467170c05a28fdd
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-07-03 13:18:06 +0000

    changes for show hide div according selection

---
+++ logicq/src/main/java/com/crm/logicq/controller/message/sms/SMSController.java
@@ -0,0 +1,46 @@
+package com.crm.logicq.controller.message.sms;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crm.logicq.model.alert.SMSDetails;
+
+@RestController
+@RequestMapping("/admin/smstemplate")
+public class SMSController {
+	
+	
+	
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SMSDetails> addSMSTemplate(@RequestBody SMSDetails smsdetails) {
+	
+		return new ResponseEntity<SMSDetails>(smsdetails, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SMSDetails> updateSMSTemplate(@RequestBody SMSDetails smsdetails ) {
+		
+		return new ResponseEntity<SMSDetails>(smsdetails, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SMSDetails> deleteSMSTemplate(@PathVariable String templateid) {
+		SMSDetails smsdetails=new SMSDetails();
+		return new ResponseEntity<SMSDetails>(smsdetails, HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(value = "/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SMSDetails> searchSMSTemplate(@PathVariable String templateid) {
+		SMSDetails smsdetails=new SMSDetails();
+		return new ResponseEntity<SMSDetails>(smsdetails, HttpStatus.OK);
+	}
+
+}
--- logicq/src/main/webapp/assets/js/services/AdminService.js
+++ logicq/src/main/webapp/assets/js/services/AdminService.js
@@ -3,13 +3,35 @@
 	'use strict';
 	angular.module('crmlogicq').factory('AdminService', ['$http' ,'AuthenticationService','AppConstants' ,function ($http ,AuthenticationService,AppConstants){
 
-	return {
+		return {
 
-		showReport: function ($scope) {
-			var url = AppConstants.hostName+AppConstants.hostPort+AppConstants.applicationName+AppConstants.showReportURL;
-			return  $http.post(url,$scope.request)
+			addSMSTemplate:function ($scope){
+				var url = AppConstants.hostName+AppConstants.hostPort+AppConstants.applicationName+AppConstants.addSMSTemplate;
+				return  $http.post(url,request)
+			},
+			
+			updateSMSTemplate: function (request) {
+				var url = AppConstants.hostName+AppConstants.hostPort+AppConstants.applicationName+AppConstants.updateSMSTemplate;
+				return  $http.post(url,request)
+
+			},
+			
+			deleteSMSTemplate: function (request) {
+				var url = AppConstants.hostName+AppConstants.hostPort+AppConstants.applicationName+AppConstants.deleteSMSTemplate;
+				return  $http.post(url,request)
+
+			}, 
+			
+			searchSMSTemplate: function (request) {
+				return $http({
+					method: 'GET',
+					url: AppConstants.hostName+AppConstants.hostPort+AppConstants.applicationName+AppConstants.searchSMSTemplate,
+					headers: {'Content-Type': 'application/json','AUTH-TOKEN':AuthenticationService.getAuthenticationToken} ,
+					dataType : 'json',
+					data : ''
+				})
+			}, 
 		}
-	}
 }]);
 
 }());
