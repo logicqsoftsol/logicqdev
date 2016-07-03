ef03b8f41ca8bd99d84e0fd2108670ea85541b7c
niharranjanbehera <niharranjanbehera@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-07-03 21:05:11 +0000

    scheduler functionality

---
+++ branches/crm-logicq/logicq/src/main/java/com/crm/logicq/service/readfile/QuartzConfiguration.java
@@ -0,0 +1,120 @@
+package com.crm.logicq.service.readfile;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+@Configuration
+@ComponentScan("com.crm.logicq.service.readfile")
+@PropertySource("classpath:scheduler.properties")
+public class QuartzConfiguration {
+
+	@Value("${scheduler.intime}")
+	private String inTime;
+	@Value("${scheduler.outtime}")
+	private String outTime;
+	@Value("${scheduler.specialmsgtime}")
+	private String specialmsgTime;
+	int startTimeHr;
+	int startTimeMin;
+
+	@Bean
+	public MethodInvokingJobDetailFactoryBean methodInvokingJobDetailFactoryBeanInTime() {
+
+		MethodInvokingJobDetailFactoryBean obj = new MethodInvokingJobDetailFactoryBean();
+		//obj.setTargetBeanName("ReadMSAccessFile");
+		//obj.setTargetBeanName("ReadFile");
+		//obj.setTargetMethod("ReadAccessFileInTime");
+		obj.setTargetBeanName("ScheduleService");
+		obj.setTargetMethod("readAccessFileInTime");
+		return obj;
+	}
+
+	@Bean
+	public MethodInvokingJobDetailFactoryBean methodInvokingJobDetailFactoryBeanOutTime() {
+
+		MethodInvokingJobDetailFactoryBean obj = new MethodInvokingJobDetailFactoryBean();
+		//obj.setTargetBeanName("ReadFile");
+		//obj.setTargetMethod("ReadAccessFileOutTime");
+		obj.setTargetBeanName("ScheduleService");
+		obj.setTargetMethod("readAccessFileOutTime");
+		return obj;
+	}
+
+	@Bean
+	public MethodInvokingJobDetailFactoryBean methodInvokingJobDetailFactorySpecial() {
+
+		MethodInvokingJobDetailFactoryBean obj = new MethodInvokingJobDetailFactoryBean();
+		//obj.setTargetBeanName("ReadFile");
+		//obj.setTargetMethod("specialJob");
+		obj.setTargetBeanName("ScheduleService");
+		obj.setTargetMethod("specialJobTime");
+		return obj;
+	}
+
+	@Bean
+	public CronTriggerFactoryBean cronTriggerFactoryBeanInTime() {
+
+		System.out.println("in Time is " + inTime);
+		int jobStartTime = Integer.valueOf(inTime);
+		startTimeHr = jobStartTime / 100;
+		startTimeMin = jobStartTime % 100;
+		CronTriggerFactoryBean stFactory = new CronTriggerFactoryBean();
+		stFactory.setJobDetail(methodInvokingJobDetailFactoryBeanInTime().getObject());
+		stFactory.setStartDelay(3000);
+		stFactory.setName("mytriggerInTime");
+		stFactory.setGroup("mygroupInTime");
+		//stFactory.setCronExpression("0 13 12 ? * MON-SUN *");
+		stFactory.setCronExpression("0 " + startTimeMin + " " + startTimeHr + " ? * MON-SUN *");
+		return stFactory;
+	}
+
+	@Bean
+	public CronTriggerFactoryBean cronTriggerFactoryBeanOutTime() {
+
+		System.out.println("outTime is " + outTime);
+		int jobStartTime = Integer.valueOf(outTime);
+		startTimeHr = jobStartTime / 100;
+		startTimeMin = jobStartTime % 100;
+		CronTriggerFactoryBean stFactory = new CronTriggerFactoryBean();
+		stFactory.setJobDetail(methodInvokingJobDetailFactoryBeanOutTime().getObject());
+		stFactory.setStartDelay(3000);
+		stFactory.setName("mytriggerOutTime");
+		stFactory.setGroup("mygroupOutTime");
+		//stFactory.setCronExpression("0 13 12 ? * MON-SUN *");
+		stFactory.setCronExpression("0 " + startTimeMin + " " + startTimeHr + " ? * MON-SUN *");
+		return stFactory;
+	}
+
+	@Bean
+	public CronTriggerFactoryBean cronTriggerFactoryBeanSpecial() {
+
+		System.out.println("SpecialTime is " + specialmsgTime);
+		int specialJobTime = Integer.valueOf(specialmsgTime);
+		startTimeHr = specialJobTime / 100;
+		startTimeMin = specialJobTime % 100;
+		CronTriggerFactoryBean stFactory = new CronTriggerFactoryBean();
+		stFactory.setJobDetail(methodInvokingJobDetailFactorySpecial().getObject());
+		stFactory.setStartDelay(3000);
+		stFactory.setName("mytriggerSpecialTime");
+		stFactory.setGroup("mygroupSpecialTime");
+		//stFactory.setCronExpression("0 13 12 ? * MON-SUN *");
+		stFactory.setCronExpression("0 " + startTimeMin + " " + startTimeHr + " ? * MON-SUN *");
+		return stFactory;
+	}
+
+	@Bean
+	public SchedulerFactoryBean schedulerFactoryBean() {
+
+		SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
+		scheduler.setTriggers(cronTriggerFactoryBeanInTime().getObject(),
+		                      cronTriggerFactoryBeanOutTime().getObject(),
+		                      cronTriggerFactoryBeanSpecial().getObject());
+		return scheduler;
+	}
+}
---
+++ branches/crm-logicq/logicq/src/main/java/com/crm/logicq/service/readfile/SchedulerService.java
@@ -0,0 +1,44 @@
+package com.crm.logicq.service.readfile;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crm.logicq.dao.readfile.ReadFile;
+import com.crm.logicq.model.CardReadDetails;
+
+@Service("ScheduleService")
+public class SchedulerService {
+
+	private ReadFile readFile;
+
+	@Autowired
+	public void setReadFile(ReadFile readFile) {
+
+		this.readFile = readFile;
+	}
+
+	public ReadFile getReadFile() {
+
+		return readFile;
+	}
+
+	public void readAccessFileInTime() {
+
+		List<CardReadDetails> inTimeList = readFile.readAccessFileInTime();
+	}
+
+	public void readAccessFileOutTime() {
+
+		List<CardReadDetails> outTimeList = readFile.readAccessFileOutTime();
+	}
+
+	public void specialJobTime() {
+
+		readFile.specialJob();
+	}
+}
+/**
+ * NIHAR 04-Jul-2016 1:37:33 am
+ */
