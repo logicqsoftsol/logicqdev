53853b0645ebee4bea00ce0642580dec74ba0e99
rakeshnayak <rakeshnayak@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-07-20 20:54:08 +0000

    Changes done related to attendance details.

--- logicq/src/main/java/com/crm/logicq/common/LogicqContextProvider.java
+++ logicq/src/main/java/com/crm/logicq/common/LogicqContextProvider.java
@@ -11,11 +11,15 @@ public class LogicqContextProvider implements ApplicationContextAware {
 
 	private static ApplicationContext context;
 	private static Map<String, Object> conversationMap;
+	private static Map<String, Object> employeeMap;
+	private static Map<String, Object> studentMap;
 
 	public void setApplicationContext(ApplicationContext appcontext) throws BeansException {
 
 		context = appcontext;
 		conversationMap = new HashMap<String, Object>();
+		employeeMap = new HashMap<String, Object>();
+		studentMap = new HashMap<String, Object>();
 	}
 	
 	public static ApplicationContext getApplicationContext() {
@@ -43,5 +47,43 @@ public class LogicqContextProvider implements ApplicationContextAware {
 		conversationMap.put(key, value);
 	}
 	
+	public static Object getElementFromEmployeeMap(String key){
+		Object element=null;
+		if(employeeMap.containsKey(key)){
+			element=employeeMap.get(key);
+		}
+	return element;
+	}
 	
+	public static Object removeElementFromEmployeeMap(String key){
+		Object element=null;
+		if(employeeMap.containsKey(key)){
+			element=employeeMap.remove(key);
+		}
+	return element;
+	}
+
+	public static void addElementToEmployeeMap(String key ,Object value){
+		employeeMap.put(key, value);
+	}
+	
+	public static Object getElementFromStudentMap(String key){
+		Object element=null;
+		if(studentMap.containsKey(key)){
+			element=studentMap.get(key);
+		}
+	return element;
+	}
+	
+	public static Object removeElementFromStudentMap(String key){
+		Object element=null;
+		if(studentMap.containsKey(key)){
+			element=studentMap.remove(key);
+		}
+	return element;
+	}
+
+	public static void addElementToStudentMap(String key ,Object value){
+		studentMap.put(key, value);
+	}
 }
