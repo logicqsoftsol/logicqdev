a123462c0e8c2fb118ee6b3c138ea005449bb2c6
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-07-29 00:22:22 +0000

    changes for please wait loading

--- logicq/src/main/webapp/assets/js/config.js
+++ logicq/src/main/webapp/assets/js/config.js
@@ -40,7 +40,7 @@
       return {
         restrict: 'E',
         replace:true,
-        template: '<div class="loading"><img src="loading_pleasewait.gif"/>LOADING...</div>',
+        template: '<div class="loading"><i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><span class="sr-only">Loading...</span></div>',
         link: function (scope, element, attr) {
               scope.$watch('loading', function (val) {
                   if (val)
