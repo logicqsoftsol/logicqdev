bfd77b4cad1a0b10461b74507e770008ce8d6485
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-08-15 21:53:05 +0000

    changes for field engg

---
+++ branches/issuetracker/src/main/webapp/assets/views/serviceengg/fieldengg/FieldEnginerController.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';
+	angular.module('issuetracker').controller('FieldEnginerController',['$scope','$rootScope','$http', '$exceptionHandler','$location','AppConstants',function ($scope,$rootScope,$http,$exceptionHandler,$location,AppConstants) {
+		
+		 
+		
+									
+	
+	}]);
+}());
