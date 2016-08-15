b5eb93c08884c585b26753f7c6ee3a66481e72d3
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-08-15 21:50:16 +0000

    changes for issue tracker

--- branches/issuetracker/src/main/webapp/assets/views/account/AccountController.js
+++ branches/issuetracker/src/main/webapp/assets/views/account/AccountController.js
@@ -1,6 +1,6 @@
 (function () {
 	'use strict';
-	angular.module('issuetracker').controller('AccountController',['$scope','$rootScope','$http', '$exceptionHandler','$location','AdminService','AttendanceService','UserService','GraphHelper','UserHelper','CalendarService','NotificationService','AppConstants',function ($scope,$rootScope,$http,$exceptionHandler,$location,AdminService,AttendanceService,UserService,GraphHelper,UserHelper,CalendarService,NotificationService,AppConstants) {
+	angular.module('issuetracker').controller('AccountController',['$scope','$rootScope','$http', '$exceptionHandler','AppConstants',function ($scope,$rootScope,$http,$exceptionHandler,AppConstants) {
 		
 		 
 		
