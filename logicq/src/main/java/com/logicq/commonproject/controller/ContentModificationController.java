b04466160fedd2385aeba1e22ee4a6c79294fa38
sudhanshulenka <sudhanshulenka@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-10-06 19:32:59 +0000

    changes for webservices

--- logicq/src/main/java/com/logicq/commonproject/controller/ContentModificationController.java
+++ logicq/src/main/java/com/logicq/commonproject/controller/ContentModificationController.java
@@ -4,16 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.logicq.commonproject.model.ContentDetails;
 import com.logicq.commonproject.service.IContentModificationService;
 
-@Controller
+@RestController
 @RequestMapping("/admin/content")
 public class ContentModificationController {
 	
@@ -24,7 +27,7 @@ public class ContentModificationController {
 	IContentModificationService contentmodificationservice;
 	
 	
-	@RequestMapping(value = "/saveOrUpdateContent", method = RequestMethod.POST)
+	@RequestMapping(value = "/saveOrUpdateContent", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ContentDetails> userRegister(@RequestBody ContentDetails contentDetails) throws Exception {
 		if(null==contentmodificationservice){
 		return 	new ResponseEntity<ContentDetails>(contentDetails, HttpStatus.BAD_REQUEST);
@@ -35,5 +38,11 @@ public class ContentModificationController {
 		return new ResponseEntity<ContentDetails>(contentDetails, HttpStatus.OK);
 	}
 	
+	
+	@RequestMapping(value = "/getTest/{data}", method = RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getTest(@PathVariable String data) throws Exception {
+		System.out.println(" Test "+ data);
+		return new ResponseEntity<String>(data, HttpStatus.OK);
+	}
 	
 }
--- logicq/src/main/java/com/logicq/commonproject/controller/FileUploadController.java
+++ logicq/src/main/java/com/logicq/commonproject/controller/FileUploadController.java
@@ -8,11 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.logicq.commonproject.helper.PropertyHelper;
@@ -21,7 +21,7 @@ import com.logicq.commonproject.service.IFileUploadService;
 import com.logicq.commonproject.vo.FileUploadVO;
 
 
-@Controller
+@RestController
 @RequestMapping("/admin/file")
 public class FileUploadController {
 
