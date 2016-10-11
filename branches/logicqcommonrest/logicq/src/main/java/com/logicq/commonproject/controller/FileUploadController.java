ce570291a0c283849c54afee89d99ba803cd981f
niharranjanbehera <niharranjanbehera@0b8ef4be-469a-43fb-8647-26fffb973b3e>
2016-10-11 19:24:37 +0000

    admin screen functionality.

--- branches/logicqcommonrest/logicq/src/main/java/com/logicq/commonproject/controller/FileUploadController.java
+++ branches/logicqcommonrest/logicq/src/main/java/com/logicq/commonproject/controller/FileUploadController.java
@@ -1,60 +1,50 @@
 package com.logicq.commonproject.controller;
 
-
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.annotation.MultipartConfig;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.logicq.commonproject.helper.PropertyHelper;
-import com.logicq.commonproject.model.FileUpload;
 import com.logicq.commonproject.service.IFileUploadService;
-import com.logicq.commonproject.vo.FileUploadVO;
-
 
 @RestController
 @RequestMapping("/admin/file")
+@MultipartConfig(fileSizeThreshold = 20971520)
 public class FileUploadController {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(FileUploadController.class);
-
+	private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 	@Autowired
 	IFileUploadService fileuploadservice;
-	
-	@RequestMapping(value = "/uploadfiles", method = RequestMethod.POST)
-	public ResponseEntity<String> userRegister(@RequestBody FileUploadVO fileuploadvo) throws Exception {
-		if (null != fileuploadvo) {
-			String filepath = PropertyHelper.loadProperty().getProperty("filepath");
-			MultipartFile[] files = fileuploadvo.getFilecontent();
-			for (MultipartFile file : files) {
-				if (!file.isEmpty()) {
-					FileUpload fileupload = new FileUpload();
-					fileupload.setCategory(fileuploadvo.getCategory());
-					fileupload.setDisplaysubtitle(fileuploadvo.getDisplaysubtitle());
-					fileupload.setDisplaytitle(fileuploadvo.getDisplaytitle());
-					fileupload.setName(file.getOriginalFilename());
-					fileupload.setPath(filepath);
-					fileupload.setSubcategory(fileuploadvo.getSubcategory());
-					fileupload.setTooltip(fileuploadvo.getTooltip());
-					fileupload.setType(file.getContentType());
-					fileupload.setFilesize(file.getSize() / 1024 + "KB");
-					fileuploadservice.uploadFile(fileupload);
-					FileCopyUtils.copy(file.getBytes(), new FileOutputStream(filepath + file.getOriginalFilename()));
-				}
-			}
-		}
 
-		return new ResponseEntity<String>("You successfully uploaded file", HttpStatus.OK);
+	@RequestMapping(value = "/uploadfiles/{findFolder}", method = { RequestMethod.POST, RequestMethod.GET })
+	public String uploadFile(@RequestParam("uploadedFile") MultipartFile uploadedFileRef, @PathVariable("findFolder") String findFolder) {
+
+		// Get name of uploaded file.		
+		String fileName = uploadedFileRef.getOriginalFilename();
+		// Path where the uploaded file will be stored.
+		String path = "D://datlib/" + findFolder + "/" + fileName;
+		// This buffer will store the data read from 'uploadedFileRef'
+		try {
+			FileCopyUtils.copy(uploadedFileRef.getBytes(), new FileOutputStream(path + uploadedFileRef.getOriginalFilename()));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return "File uploaded successfully! ";
 	}
-	
 }
